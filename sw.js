3e84ab1d03f3808b36297e3282bb91deb074d41a
asunki-313 <azzam.a.sunki@gmail.com>
2026-08-10 11:59:51 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ikhtabr-v13-2500';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='ikhtabr-v13-pwa-2500';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./thinking_person.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
