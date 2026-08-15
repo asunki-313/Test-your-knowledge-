65be7a803cd62f550de1d5132d3b3e2a89411faf
asunki-313 <azzam.a.sunki@gmail.com>
2026-08-15 18:15:36 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME="ikhtabr-v16-10-beta1";
+const CACHE_NAME="ikhtabr-v16-11-pwa";
 const APP_SHELL=["./","./index.html","./manifest.webmanifest","./thinking_person.png","./quiz_background_v1415.png","./data/questions_cumulative_3500.json"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL).catch(()=>{})));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;const u=e.request.url;if(u.includes("firestore.googleapis.com")||u.includes("googleapis.com/identitytoolkit")||u.includes("securetoken.googleapis.com"))return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE_NAME).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));});
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open('ikhtabr-v16-10-beta1').then(c => c.add('assets/battle_of_knowledge_v1610.png')).catch(()=>{}));
+  event.waitUntil(caches.open('ikhtabr-v16-11-pwa').then(c => c.add('assets/battle_of_knowledge_v1611.png')).catch(()=>{}));
 });
